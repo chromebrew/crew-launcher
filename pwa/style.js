d714fc38c1d5527b38d68cdd91dc42aa894fdb11
supechicken <supechicken666@gmail.com>
2021-09-20 21:18:27 +0800

    Update style.js

--- pwa/style.js
+++ pwa/style.js
@@ -4,5 +4,5 @@ const url = new URL(window.location),
 
 if (dark_mode == '1') {
   document.body.style.backgroundColor = '#121212';
-  document.p.style.color = 'white';
+  document.body.style.color = 'white';
 }
